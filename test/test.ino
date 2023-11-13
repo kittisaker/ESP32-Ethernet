9d277489d1839411a9c1ae964f1d4b126ef07659
kittisaker <kittisak.hanheam@gmail.com>
2023-11-13 22:58:46 +0700

    update chapter-4

--- test/test.ino
+++ test/test.ino
@@ -2,43 +2,38 @@
 #include <Ethernet.h>
 
 byte MAC[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
 EthernetClient ethClient;
 
 void setup() {
   Serial.begin(115200);
+
   Ethernet.init(5);
 
-  if (Ethernet.begin(MAC)) {
+  if (Ethernet.begin(MAC)) {  // Dynamic IP setup
     Serial.println("DHCP OK!");
+
+    // Get and print the IP address
+    IPAddress ip = Ethernet.localIP();
+    Serial.print("My IP address: ");
+    Serial.println(ip);
+
   } else {
-    handleEthernetError();
+    Serial.println("Failed to configure Ethernet using DHCP");
+
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1);
+      }
+    }
+
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
   }
 }
 
 void loop() {
-  testEthernetConnection();
-  delay(10000); // Delay between tests (10 seconds)
-}
-
-void handleEthernetError() {
-  Serial.println("Failed to configure Ethernet using DHCP");
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.");
-    while (true) {
-      delay(1); // Halt the program
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-}
-
-void testEthernetConnection() {
-  // Try to connect to a known server (e.g., Google's public DNS server)
-  if (ethClient.connect("8.8.8.8", 80)) {
-    Serial.println("Connection test successful");
-    ethClient.stop(); // Disconnect after the test
-  } else {
-    Serial.println("Connection test failed");
-  }
+  // The loop function is left empty if you don't need to perform any repeated action.
 }
